b0fd57439dcbbb3709b6593eeab0ba287db29dac
hiramoubin9 <hiramoubin9@gmail.com>
2026-08-25 16:56:55 -0400

    Complete Redux shopping cart functionality

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,29 +9,58 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    let total = 0;
+
+    cart.forEach(item => {
+      const quantity = item.quantity;
+      const cost = parseFloat(item.cost.substring(1));
+  
+      total += cost * quantity;
+    });
+  
+    return total;
   };
 
   const handleContinueShopping = (e) => {
-   
+    onContinueShopping(e);
   };
 
 
 
   const handleIncrement = (item) => {
+    dispatch(
+        updateQuantity({
+          name: item.name,
+          quantity: item.quantity + 1
+        })
+      );
   };
 
   const handleDecrement = (item) => {
-   
+    if (item.quantity > 1) {
+        dispatch(
+          updateQuantity({
+            name: item.name,
+            quantity: item.quantity - 1
+          })
+        );
+      } else {
+        dispatch(removeItem(item.name));
+      }
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item.name));
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    const price = parseFloat(item.cost.substring(1));
+  return price * item.quantity;
   };
-
+  const handleCheckoutShopping = (e) => {
+    alert('Functionality to be added for future reference');
+  };
   return (
     <div className="cart-container">
       <h2 style={{ color: 'black' }}>Total Cart Amount: ${calculateTotalAmount()}</h2>
@@ -57,7 +86,9 @@ const CartItem = ({ onContinueShopping }) => {
       <div className="continue_shopping_btn">
         <button className="get-started-button" onClick={(e) => handleContinueShopping(e)}>Continue Shopping</button>
         <br />
-        <button className="get-started-button1">Checkout</button>
+        <button className="get-started-button1" onClick={(e) => handleCheckoutShopping(e)}>
+  Checkout
+</button>
       </div>
     </div>
   );
